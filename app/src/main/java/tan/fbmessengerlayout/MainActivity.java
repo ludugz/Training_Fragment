0a3852e4563d5408053229e55137e5ddaaea4835
Truong Nhat Tan <Tan@NALS-THAMTRA-MAC.local>
2018-04-10 14:25:00 +0700

    ToolBar can disappear when scroll

--- app/src/main/java/tan/fbmessengerlayout/MainActivity.java
+++ app/src/main/java/tan/fbmessengerlayout/MainActivity.java
@@ -4,9 +4,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -16,11 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tan.fbmessengerlayout.Adapter.MyPagerAdapter;
-import tan.fbmessengerlayout.Adapter.MyRecyclerAdapter;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-
+    public static List<Integer> dataList = new ArrayList<>();
 
     @ViewById(R.id.view_pager)
     ViewPager mViewPager;
@@ -28,15 +25,31 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.tab_layout)
     TabLayout mTabLayout;
 
+    @ViewById(R.id.tool_bar)
+    Toolbar mToolBar;
+
 
     @AfterViews
-    void updateFragment(){
+    void updateFragment() {
+
+
+        setSupportActionBar(mToolBar);
+        // Update DataList for FragmentOne
+        updateDataList();
         // Handle ViewPager
         PagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(pagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
 
-
     }
 
+    private void updateDataList() {
+        dataList.add(R.drawable.album_name_1);
+        dataList.add(R.drawable.album_name_3);
+        dataList.add(R.drawable.album_name_4);
+        dataList.add(R.drawable.album_name_5);
+        dataList.add(R.drawable.album_name_6);
+        dataList.add(R.drawable.album_name_7);
+        dataList.add(R.drawable.album_name_8);
+    }
 }
